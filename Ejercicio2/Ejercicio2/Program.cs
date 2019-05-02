f4b36be41b401a92870b14be8ba2126e725de15b
Mario Márquez <48485832+Purrum07@users.noreply.github.com>
2019-05-02 13:29:11 -0600

    Ejercicio2

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program 
+    {
+        public static void  insertionSort<T> (T[] arr, T n) 
+        {
+            T i, key, j;
+            for (i = 1; i < n; i++)
+            {
+                key = arr[i];
+                j = i - 1;
+
+                /* Move elements of arr[0..i-1], that are
+                   greater than key, to one position ahead
+                   of their current position */
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+
+           
+        }
+    }
+}
